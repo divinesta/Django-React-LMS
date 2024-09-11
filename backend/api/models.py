9203327050728706b2b6715a654b6e652b4891e5
Emilola Divine <emiloladivine123@gmail.com>
2024-09-11 11:27:39 +0100

    Course, Category and Teacher Models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,105 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+
+from userauths.models import User, Profile
+from shortuuid.django_fields import ShortUUIDField
 
 # Create your models here.
+
+
+LANGUAGE = (
+    ("English", "English"),
+    ("French", "French"),
+    ("Spanish", "Spanish"),
+)
+
+LEVEL = (
+    ("Beginner", "Beginner"),
+    ("Intermediate", "Intermediate"),
+    ("Advanced", "Advanced"),
+)
+
+STATUS = (
+    ("Draft", "Draft"),
+    ("Disabled", "Disabled"),
+    ("Published", "Published"),
+)
+
+PLATFORM = (
+    ("Review", "Review"),
+    ("Disabled", "Disabled"),
+    ("Rejected", "Rejected"),
+    ("Draft", "Draft"),
+    ("Published", "Published"),
+)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="course-file", null=True, blank=True, default="default.jpg")
+    full_name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=100, null=True, blank=True)
+    facebook = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    linkedin = models.URLField(null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+
+    def students(self):
+        return CartOrderItem.objects.filter(teacher=self)
+    
+    def courses(self):
+        return Course.objects.filter(teacher=self)
+    
+    def review(self):
+        return Course.objects.filter(teacher=self)
+    
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="course-file", null=True, blank=True, default="category.jpg")
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Category"
+        ordering = ["title"]
+    
+    def __str__(self):
+        return self.title
+    
+    def course_count(self):
+        return Course.objects.filter(category=self).count()
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+class Course(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    file = models.FileField(upload_to="course-file", null=True, blank=True)
+    image = models.FileField(upload_to="course-file", null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    language = models.CharField(choices=LANGUAGE, default="English")
+    level = models.CharField(choices=LEVEL, default="Beginner")
+    platform_status = models.CharField(choices=PLATFORM, default="Published")
+    teacher_course_status = models.CharField(choices=STATUS, default="Draft")
+    featured = models.BooleanField(default=False)
+    course_id = ShortUUIDField(unique=True, length=6, max_length=20, alphabet="abcdefghijklmn0123456789")
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Course, self).save(*args, **kwargs)
+
+
